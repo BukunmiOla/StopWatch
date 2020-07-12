fd8da17de4c135b122796a121e6816f20440240d
BukunmiOla <ebukunmiola@gmail.com>
2020-07-12 13:34:22 +0100

    button color preference set, start stop string resource added and start stop flip functionality fixed

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button startStopBtn = findViewById(R.id.start_stop_btn);
+    Button startStopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startStopBtn = findViewById(R.id.start_stop_btn);
+        startStopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (stat()){
+                    start();
+                }else{
+                    stop();
+                }
+            }
+        });
+    }
+
+    private void start() {
+        startStopBtn.setText(R.string.stop);
+        startStopBtn.setTextColor(Color.parseColor("#EF0808"));
+    }
+
+    private void stop() {
+        startStopBtn.setText(R.string.start);
+        startStopBtn.setTextColor(Color.parseColor("#05E10E"));
+    }
+
+    private boolean stat() {
+        String statusText = startStopBtn.getText().toString();
+        return statusText.equalsIgnoreCase("start");
+
     }
 }
