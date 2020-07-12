8ac9de6a36b409d8e34e755740dd13b3deb6483e
BukunmiOla <ebukunmiola@gmail.com>
2020-07-12 18:35:36 +0100

    inability to stop bug fixed

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -12,11 +12,13 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     Button startStopBtn;
     Chronometer watch;
     Handler handler;
     Long tMilliSec, tBuff, tUpdate,tStart =0L;
     int hour, min, sec, milliSec;
+    Boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         watch = findViewById(R.id.watch_chrm);
         handler = new Handler();
         startStopBtn = findViewById(R.id.start_stop_btn);
+
         startStopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,18 +51,25 @@ public class MainActivity extends AppCompatActivity {
     public Runnable startRun = new Runnable() {
         @Override
         public void run() {
-            tMilliSec = SystemClock.uptimeMillis()-tStart;
-            tUpdate = tBuff = tMilliSec;
-            sec = (int) (tUpdate/1000);
-            min = sec/60;
-            hour = min/60;
-            min = min%60;
-            sec = sec%60;
-            milliSec = (int) (tUpdate%100);
-            String display = String.format("%02d",hour)+":"+String.format("%02d",min)+":"+
-                    String.format("%02d",sec)+":"+ String.format("%02d",milliSec);
-            watch.setText(display);
-            handler.postDelayed(this,60);
+            if (!running) {
+                tMilliSec = tBuff = tUpdate = tStart =0L;
+                hour = min = sec = milliSec = 0;
+                String stopText = "00:00:00:00";
+                watch.setText(stopText);
+            } else {
+                tMilliSec = SystemClock.uptimeMillis() - tStart;
+                tUpdate = tBuff = tMilliSec;
+                sec = (int) (tUpdate / 1000);
+                min = sec / 60;
+                hour = min / 60;
+                min = min % 60;
+                sec = sec % 60;
+                milliSec = (int) (tUpdate % 100);
+                String display = String.format("%02d", hour) + ":" + String.format("%02d", min) + ":" +
+                        String.format("%02d", sec) + ":" + String.format("%02d", milliSec);
+                watch.setText(display);
+                handler.postDelayed(this, 60);
+            }
         }
     };
 
@@ -72,26 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void startRunning() {
         tStart = SystemClock.uptimeMillis();
+        running =true;
         handler.postDelayed(startRun,0);
         watch.start();
-
-
-
     }
 
     private void stop() {
         startStopBtn.setText(R.string.start);
         startStopBtn.setTextColor(Color.parseColor("#05E10E"));
-        tMilliSec = 0L;
-        tBuff = 0L;
-        tUpdate = 0L;
-        tStart =0L;
-        hour = 0;
-        min =0;
-        sec = 0;
-        milliSec = 0;
-        String stopText = "00:00:00:00";
-        watch.setText(stopText);
+        running = false;
+        handler.removeCallbacks(startRun);
     }
 
     private boolean stat() {
@@ -111,5 +111,9 @@ public class MainActivity extends AppCompatActivity {
         minuteTv.setText(R.string.zero);
         secondsTv.setText(R.string.zero);
         milliSecondsTv.setText(R.string.zero);
+
+        watch.stop();
+        String stopText = "00:00:00:00";
+        watch.setText(stopText);
     }
 }
