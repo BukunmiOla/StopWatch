882ed49ebae3574f397bf603f1f6c2c1cb73c1dc
BukunmiOla <ebukunmiola@gmail.com>
2020-07-12 17:42:47 +0100

    chronometer added with format 00:00:00:00 and functionality added

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button startStopBtn;
+    Chronometer watch;
+    Handler handler;
+    Long tMilliSec, tBuff, tUpdate,tStart =0L;
+    int hour, min, sec, milliSec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        watch = findViewById(R.id.watch_chrm);
+        handler = new Handler();
         startStopBtn = findViewById(R.id.start_stop_btn);
         startStopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,16 +45,53 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public Runnable startRun = new Runnable() {
+        @Override
+        public void run() {
+            tMilliSec = SystemClock.uptimeMillis()-tStart;
+            tUpdate = tBuff = tMilliSec;
+            sec = (int) (tUpdate/1000);
+            min = sec/60;
+            hour = min/60;
+            min = min%60;
+            sec = sec%60;
+            milliSec = (int) (tUpdate%100);
+            String display = String.format("%02d",hour)+":"+String.format("%02d",min)+":"+
+                    String.format("%02d",sec)+":"+ String.format("%02d",milliSec);
+            watch.setText(display);
+            handler.postDelayed(this,60);
+        }
+    };
 
     private void start() {
         startStopBtn.setText(R.string.stop);
         startStopBtn.setTextColor(Color.parseColor("#EF0808"));
+        startRunning();
+
+    }
+
+    private void startRunning() {
+        tStart = SystemClock.uptimeMillis();
+        handler.postDelayed(startRun,0);
+        watch.start();
+
+
 
     }
 
     private void stop() {
         startStopBtn.setText(R.string.start);
         startStopBtn.setTextColor(Color.parseColor("#05E10E"));
+        tMilliSec = 0L;
+        tBuff = 0L;
+        tUpdate = 0L;
+        tStart =0L;
+        hour = 0;
+        min =0;
+        sec = 0;
+        milliSec = 0;
+        String stopText = "00:00:00:00";
+        watch.setText(stopText);
     }
 
     private boolean stat() {
